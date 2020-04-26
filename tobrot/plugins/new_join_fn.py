620cd91ec4fa3df4dff4229b1e28f82cc9141d10
MASK-droid <64348197+MASK-droid@users.noreply.github.com>
2020-04-26 15:19:41 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -47,8 +47,8 @@ async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
+            text="join my channel I will upload",
+            url="https://t.me/apkapkap"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
